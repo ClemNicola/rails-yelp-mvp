9ada7e26ad28127f06505c7d7d57197eb53e4fea
Clement Nicolas <clementnicolasprobis@gmail.com>
2023-11-15 15:11:11 +0100

    MVP YELP

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,34 @@
+class ReviewsController < ApplicationController
+
+  before_action :set_restaurant, only: %i[new create destroy]
+  def index
+    @reviews = Review.all
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant), status: :see_other
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+end
